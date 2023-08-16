aa86339110a1f7c264ca9259401a69d4750c7796
KenShyNoShana <ReneAslanov02@gmail.com>
2023-08-16 18:46:56 +0200

    fix: Connection-String is no longer exposed

--- backend/database.ts
+++ backend/database.ts
@@ -4,8 +4,8 @@ import fs from "fs";
 
 dotenv.config();
 
-const {DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE} = process.env;
-console.log(DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE);
+const {DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE, CONNECTION_STRING} = process.env;
+console.log(DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE, CONNECTION_STRING);
 
 //console.log(fs.readFileSync('./SSL-certificate/root-certs.crt').toString());
 
@@ -14,7 +14,7 @@ export const client = new pg.Pool({
     password: DB_PASSWORD,
     host: DB_HOST,
     database: DB_DATABASE,
-    connectionString: "postgres://my-unsplash-main-db-06810274662d31db1:FBQTcEVdETRg85VmY3td2eZNs8UByK@user-prod-us-east-2-1.cluster-cfi5vnucvv3w.us-east-2.rds.amazonaws.com:5432/my-unsplash-main-db-06810274662d31db1",
+    connectionString: CONNECTION_STRING,
     ssl: {
         rejectUnauthorized: false,
         ca: fs.readFileSync('./SSL-certificate/root-certs.crt').toString(),
